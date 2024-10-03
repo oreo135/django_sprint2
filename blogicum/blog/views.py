9aa1df2bd7754a653d2e7eb1d35c738fb1e9aca4
Ольга Кузнецова <dljapythonplus@yandex.kz>
2024-10-03 22:03:09 +0200

    pip8 okay

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -53,12 +53,12 @@ def index(request):
 
 def posts_detail(request, pk):
     template = 'blog/detail.html'
-    context = {'posts': posts[pk] }
+    context = {'posts': posts[pk]}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
-    filtered_posts = [post for post in posts 
+    filtered_posts = [post for post in posts
                       if post['category'] == category_slug]
     template = 'blog/category.html'
     context = {'posts': filtered_posts}
